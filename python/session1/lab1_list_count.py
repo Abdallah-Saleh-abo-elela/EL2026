60c8afc915ae0d30f52bec863bffbfc691e89876
Abdallah-Saleh-abo-elela <abdallahsalehmohamed393@gmail.com>
2026-06-21 20:41:26 +0300

    Solve session1 lab1

--- python/session1/lab1_list_count.py
+++ python/session1/lab1_list_count.py
@@ -3,6 +3,8 @@
 
 def count(lst):
     """Write your solution here. Don't forget to return the result at the end...."""
+    print(lst.count(4))
+    return lst.count(4)
 
 
 if __name__ == "__main__":
