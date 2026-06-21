177de82d3f6877ab1f0abeb0574af7e6748a45d6
Abdallah-Saleh-abo-elela <abdallahsalehmohamed393@gmail.com>
2026-06-21 21:02:04 +0300

    Solve session1 lab1 and lab5

--- python/session1/lab5_accumulator.py
+++ python/session1/lab5_accumulator.py
@@ -3,6 +3,11 @@
 
 def compute(number):
     """write your soulution Here Sum 1 … n — Compute and display the running total."""
+    total = 0
+    for i in range(1, number + 1):
+        total += i
+        print(f"Running total after adding {i}: {total}")
+    return total
 
 
 if __name__ == "__main__":
